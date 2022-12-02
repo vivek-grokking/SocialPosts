db90745f34d3f55c2c767c808f490bc45ce85371
vivek <bit.viveksh@gmail.com>
2022-12-02 15:37:06 +0100

    use env variables

--- app/database.py
+++ app/database.py
@@ -1,14 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-#from sqlalchemy import 
+from .config import settings 
 
 HOST = 'localhost'
 DATABASE = 'social-fastapi'
 USER = 'postgres'
 PASSWORD = ''
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}/{DATABASE}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from .routers import post, user, auth
 from . import models
 from .database import engine
 from fastapi import FastAPI
+from .config import settings
 
 # creates the tables from 'models' if not exists
 models.Base.metadata.create_all(bind=engine)
@@ -12,6 +13,6 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 
-@app.get("/")
+@app.get("/") 
 def hello_world():
     return {"message": "Hello there !)"}
